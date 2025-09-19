52b19a408a99ff8a70584775f7a0137a055925e8
copilot0805 <nguyentrungkien2005.gl@gmail.com>
2025-09-19 13:50:00 +0700

    Init for ex3

--- STM32PROJECT/Core/Src/main.c
+++ STM32PROJECT/Core/Src/main.c
@@ -92,19 +92,80 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  uint16_t led_status = 0;
+  uint16_t sec = 0;
+
+  enum Color {
+      RED,
+      GREEN,
+      YELLOW,
+  };
+
+  enum Color current_color_1 = GREEN;
+  enum Color current_color_2 = RED;
   while (1)
   {
-	  if (led_status == 0) {
-		  HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, RESET);
-		  HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, SET);
-		  led_status = 1;
-	  } else {
-		  HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, SET);
-		  HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, RESET);
-		  led_status = 0;
-	  }
-	  HAL_Delay(2000);
+	    // TRAFFIC LIGHT 1
+	    if(sec < 3) {
+	        current_color_1 = GREEN;
+	    }
+	    else if(sec >= 3 && sec < 5) {
+	        current_color_1 = YELLOW;
+	    }
+	    else {
+	        current_color_1 = RED;
+	    }
+
+	    switch(current_color_1) {
+	    case RED:
+	        HAL_GPIO_WritePin(LED_RED_1_GPIO_Port, LED_RED_1_Pin, SET);
+	        HAL_GPIO_WritePin(LED_GREEN_1_GPIO_Port, LED_GREEN_1_Pin, RESET);
+	        HAL_GPIO_WritePin(LED_YELLOW_1_GPIO_Port, LED_YELLOW_1_Pin, RESET);
+	        break;
+	    case GREEN:
+	        HAL_GPIO_WritePin(LED_GREEN_1_GPIO_Port, LED_GREEN_1_Pin, SET);
+	        HAL_GPIO_WritePin(LED_RED_1_GPIO_Port, LED_RED_1_Pin, RESET);
+	        HAL_GPIO_WritePin(LED_YELLOW_1_GPIO_Port, LED_YELLOW_1_Pin, RESET);
+	        break;
+	    case YELLOW:
+	        HAL_GPIO_WritePin(LED_YELLOW_1_GPIO_Port, LED_YELLOW_1_Pin, SET);
+	        HAL_GPIO_WritePin(LED_GREEN_1_GPIO_Port, LED_GREEN_1_Pin, RESET);
+	        HAL_GPIO_WritePin(LED_RED_1_GPIO_Port, LED_RED_1_Pin, RESET);
+	        break;
+	    }
+
+	    // TRAFFIC LIGHT 2
+	    if(sec < 5) {
+	        current_color_2 = RED;
+	    }
+	    else if(sec >= 5 && sec < 7) {
+	        current_color_2 = GREEN;
+	    }
+	    else {
+	        current_color_2 = YELLOW;
+	    }
+
+	    switch(current_color_2) {
+	    case RED:
+	        HAL_GPIO_WritePin(LED_RED_2_GPIO_Port, LED_RED_2_Pin, SET);
+	        HAL_GPIO_WritePin(LED_GREEN_2_GPIO_Port, LED_GREEN_2_Pin, RESET);
+	        HAL_GPIO_WritePin(LED_YELLOW_2_GPIO_Port, LED_YELLOW_2_Pin, RESET);
+	        break;
+	    case GREEN:
+	        HAL_GPIO_WritePin(LED_GREEN_2_GPIO_Port, LED_GREEN_2_Pin, SET);
+	        HAL_GPIO_WritePin(LED_RED_2_GPIO_Port, LED_RED_2_Pin, RESET);
+	        HAL_GPIO_WritePin(LED_YELLOW_2_GPIO_Port, LED_YELLOW_2_Pin, RESET);
+	        break;
+	    case YELLOW:
+	        HAL_GPIO_WritePin(LED_YELLOW_2_GPIO_Port, LED_YELLOW_2_Pin, SET);
+	        HAL_GPIO_WritePin(LED_GREEN_2_GPIO_Port, LED_GREEN_2_Pin, RESET);
+	        HAL_GPIO_WritePin(LED_RED_2_GPIO_Port, LED_RED_2_Pin, RESET);
+	        break;
+	    }
+
+	    if(sec > 8) sec = 0;
+	    ++sec;
+
+	    HAL_Delay(1000);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -164,10 +225,13 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, LED_RED_Pin|LED_YELLOW_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, LED_GREEN_1_Pin|LED_YELLOW_1_Pin|LED_RED_1_Pin|LED_GREEN_2_Pin
+                          |LED_YELLOW_2_Pin|LED_RED_2_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : LED_RED_Pin LED_YELLOW_Pin */
-  GPIO_InitStruct.Pin = LED_RED_Pin|LED_YELLOW_Pin;
+  /*Configure GPIO pins : LED_GREEN_1_Pin LED_YELLOW_1_Pin LED_RED_1_Pin LED_GREEN_2_Pin
+                           LED_YELLOW_2_Pin LED_RED_2_Pin */
+  GPIO_InitStruct.Pin = LED_GREEN_1_Pin|LED_YELLOW_1_Pin|LED_RED_1_Pin|LED_GREEN_2_Pin
+                          |LED_YELLOW_2_Pin|LED_RED_2_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
